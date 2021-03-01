54a97c77692bd47fe909e04bb147aaa9c1dc32a5
JakoPapaFrita <jacobouribe@inemjose.edu.co>
2021-02-28 20:20:31 -0500

    test

--- src/components/ControladorPresupuesto.jsx
+++ src/components/ControladorPresupuesto.jsx
@@ -11,7 +11,7 @@ const ControladorPresupuesto = ({presupuestoGeneral}) => {
     if(presupuestoRestanteInicial < 1){
         presupuestoRestanteInicial = presupuestoGeneral;
     }
-    let variable = 'variable1'
+    let variable = 'variable2'
     const [presupuestoInicial] = useState(presupuestoGeneral);
     const [presupuestoRestante, setPresupuestoRestante] = useState(presupuestoRestanteInicial);
     const [gasto, setGasto] = useState({
